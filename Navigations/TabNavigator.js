74c8db93777a1cb7272c96977327e59dd810f33b
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-15 22:07:09 +0300

    add BottomTabNavigator

---
+++ Navigations/TabNavigator.js
@@ -0,0 +1,50 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchStackScreen from "./StackNavigators/SearchStackScreen";
+import ListStackScreen from "./StackNavigators/ListStackScreen";
+import React from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+    return (
+
+      <Tab.Navigator initialRouteName="Search" screenOptions={{ headerShown: false }} >
+          
+        {/* <Tab.Screen
+            options={{
+                tabBarLabel:"Search",
+                tabBarLabelStyle:{color:"black"},
+                tabBarIcon: ({color,size}) => (
+                    <FontAwesome name='search' 
+                    color={"black"} size={size}
+                     />
+                ),
+                tabBarActiveBackgroundColor:"#cdcfd1"
+            }}
+            color="black"
+            name="Search" 
+            component={SearchStackScreen} 
+        />
+
+        <Tab.Screen
+            options={{
+                tabBarLabel:"List",
+                tabBarLabelStyle:{color:"black"},
+                tabBarIcon: ({color, size}) => (
+                    <FontAwesome name='list-ul'
+                    color={"black"} size={size} />
+                ),
+                tabBarActiveBackgroundColor:"#cdcfd1"
+            }} 
+            name="List"
+            component={ListStackScreen} 
+        /> */}
+        
+      </Tab.Navigator>
+
+    );
+}
+
+export default TabNavigator;
