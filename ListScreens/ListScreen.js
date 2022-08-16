e8ac05a8286c9723d1d6f1735bab4cc95aaed948
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-16 22:12:00 +0300

    add list screen with dropDown

---
+++ ListScreens/ListScreen.js
@@ -0,0 +1,55 @@
+import {Text, View, StyleSheet, FlatList, Linking, SafeAreaView, ActivityIndicator, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Dropdown} from 'react-native-element-dropdown';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+function ListScreen({navigation}) {
+
+  const [data, setData] = useState();
+  const [filteredData, setFilteredData] = useState(null);
+  const [categArray, setCategArray] = useState([]);
+  const filteredArray = [];
+  const [error, setError]= useState(false)
+  const [loading, setLoading]  = useState(true)
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+      <SafeAreaView style={styles.container}>
+            <Text style={[styles.label]}></Text>
+            <Dropdown
+                style={[styles.dropdown]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                search
+                data={categArray}
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder={!isFocus ? 'Select Category' : '...'}
+                searchPlaceholder="Search..."
+                value={value}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={item => {
+                setValue(item.value);
+                setIsFocus(false);
+                filter(item.value);
+                }}
+                renderLeftIcon={() => (
+                <AntDesign
+                    style={styles.icon}
+                    color={'black'}
+                    name="Safety"
+                    size={20}
+                />
+                )}
+            />
+
+      </SafeAreaView>
+  );
+}
+
+export default ListScreen;
